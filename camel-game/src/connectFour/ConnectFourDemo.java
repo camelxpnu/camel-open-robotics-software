8da0733863132df1fb98051f22fb0fd893e565d9
Inho Lee <Inho Lee@ilee-pc>
2021-08-08 00:57:18 +0900

    Tested basic functionality of game players.

--- camel-game/src/connectFour/ConnectFourDemo.java
+++ camel-game/src/connectFour/ConnectFourDemo.java
@@ -1,6 +1,16 @@
 package connectFour;
 
+import connectFour.Player.ComputerPlayer;
+import connectFour.Player.HumanPlayer;
+
 public class ConnectFourDemo
 {
-
+	public static void main(String[] args)
+	{
+		HumanPlayer human = new HumanPlayer(1);
+		human.play(1, 2, 3, 4, 7);
+		
+		ComputerPlayer computer = new ComputerPlayer(2);
+		computer.play(1, 2, 3, 4, 7);
+	}
 }
