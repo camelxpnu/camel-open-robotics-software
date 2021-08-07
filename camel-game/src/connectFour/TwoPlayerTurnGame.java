17d3f3c35128e0131fcf727db44215b35ff34763
Inho Lee <Inho Lee@ilee-pc>
2021-08-08 01:52:48 +0900

    Created turn game for two players.

---
+++ camel-game/src/connectFour/TwoPlayerTurnGame.java
@@ -0,0 +1,38 @@
+package connectFour;
+
+import connectFour.Player.GamePlayer;
+
+public abstract class TwoPlayerTurnGame implements TurnGameInterface
+{
+	private final GamePlayer firstPlayer;
+	private final GamePlayer secondPlayer;
+
+	protected GamePlayer currentPlayer;
+
+	public TwoPlayerTurnGame(GamePlayer firstPlayer, GamePlayer secondPlayer)
+	{
+		this.firstPlayer = firstPlayer;
+		this.secondPlayer = secondPlayer;
+		currentPlayer = firstPlayer;
+	}
+
+	protected void changeCurrentPlayer()
+	{
+		if (currentPlayer == firstPlayer)
+		{
+			currentPlayer = secondPlayer;
+		} else
+		{
+			currentPlayer = firstPlayer;
+		}
+	}
+
+	@Override
+	public void play()
+	{
+		provideAvailableActionsAndPlay();
+		changeCurrentPlayer();
+	}
+
+	public abstract void provideAvailableActionsAndPlay();
+}
